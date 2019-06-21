202582f0ec17a60768d6e5930f7013c5af2970df
alessanderleite <alessander.logistica@gmail.com>
2019-06-21 16:53:43 -0300

    add the right annotation

--- src/main/java/br/com/alessanderleite/api/exception/ResourceNotFoundException.java
+++ src/main/java/br/com/alessanderleite/api/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.alessanderleite.api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception {
 
 	/**
