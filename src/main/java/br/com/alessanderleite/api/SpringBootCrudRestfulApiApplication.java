d6287d7cd58135a8dab4932152b417b59d75a967
alessanderleite <alessander.logistica@gmail.com>
2019-06-21 13:42:00 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/api/SpringBootCrudRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestfulApiApplication.class, args);
+	}
+
+}
